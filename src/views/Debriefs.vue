6d801e80a38c368151bdd0aec7aac562bd235917
Arif Kurkchi <justnero.ru@yandex.ru>
2024-10-31 15:32:32 +0300

    Add project 13

--- src/views/Debriefs.vue
+++ src/views/Debriefs.vue
@@ -9,7 +9,11 @@
       <div class="project-reponse" v-if="activeProject === project.title">
         <div v-for="feedback in project.questions">
           <h5>{{ feedback.question }}</h5>
-          <div v-html="feedback.answer"></div>
+          <div v-html="feedback.answer" v-if="typeof feedback.answer === 'string'"></div>
+          <ol v-else-if="Array.isArray(feedback.answer)">
+            <li v-for="answer in feedback.answer" :key="answer" v-html="answer"></li>
+          </ol>
+          <div v-else>---</div>
         </div>
       </div>
     </div>
@@ -17,6 +21,18 @@
 </template>
 
 <script setup>
+import debriefImage1 from "@/assets/debriefs/13-01.png";
+import debriefImage2 from "@/assets/debriefs/13-02.png";
+import debriefImage3 from "@/assets/debriefs/13-03.png";
+import debriefImage4 from "@/assets/debriefs/13-04.png";
+import debriefImage5 from "@/assets/debriefs/13-05.png";
+import debriefImage6 from "@/assets/debriefs/13-06.png";
+import debriefImage7 from "@/assets/debriefs/13-07.png";
+import debriefImage8 from "@/assets/debriefs/13-08.png";
+import debriefImage9 from "@/assets/debriefs/13-09.png";
+import debriefImage10 from "@/assets/debriefs/13-10.png";
+import debriefImage11 from "@/assets/debriefs/13-11.png";
+import debriefImage12 from "@/assets/debriefs/13-12.png";
 import { ref } from "vue";
 const projectFeedback = ref([
   {
@@ -105,6 +121,36 @@ const projectFeedback = ref([
         question: "What did you find confusing? Did you encounter any issues?",
         answer:
           "<ul><li style='margin-bottom:10px;'>Values from 1000 displays NaN in the amount input field. Seems there's a bug in instruments for that length of value</li> <li style='margin-bottom:10px;'>We went over the time by 30 mins.</li></ul>",
+      },
+    ],
+  },
+  {
+    title: "Project 13",
+    questions: [
+      {
+        question: "Which teammates executed the project?",
+        answer: "Team members: Olasunkanmi, and Arif",
+      },
+      {
+        question: "What made you smile?",
+        answer: "",
+      },
+      {
+        question: "What did you find confusing? Did you encounter any issues?",
+        answer: [
+          'We were not able to find the first clue on how to adjust available payment methods by searching documentation. Queries like `specific payment methods` or `available payment methods` return confusing results. While `display payment methods` got us to the target docs page via 2 clicks. Probably keywords search would help here.',
+          "The spelling is different on a single screen which is confusing<br><img src=" + debriefImage1 + " alt='debrief image' style='width: 100%; margin-top: 10px;' />",
+          "RSelect element with tagging enabled should not propose to create a tag if strict match was found<br><img src=" + debriefImage2 + " alt='debrief image' style='width: 100%; margin-top: 10px;' />",
+          `"Add another payment method is not working if no payment methods available anymore. This is not stated anywhere, and the button itself seems completely active, just not working for some reason.<br><img src="${debriefImage3}" alt='debrief image' style='width: 100%; margin-top: 10px;' />`,
+          `Payment methods select at the Gateway account edit screen has multiple "bank-transfer" options with no clear distinction between them except of the suffix number. It's not clear which one to choose.<br><img src="${debriefImage4}" alt='debrief image' style='width: 100%; margin-top: 10px;' />`,
+          `Gateway account ID field is prefilled with the gateway name as a value which can already be occupied leading to error message.<br><img src="${debriefImage5}" alt='debrief image' style='width: 100%; margin-top: 10px;' />`,
+          `There is no way to open sidebar inside the API documentation.<br><img src="${debriefImage6}" alt='debrief image' style='width: 100%; margin-top: 10px;' />`,
+          `There is no way to prioritize payment card brands in the adjust ready to pay payment methods list. Had to disable other brands at the gateway account level.<br><img src="${debriefImage7}" alt='debrief image' style='width: 100%; margin-top: 10px;' />`,
+          `When dragging items in the list, the item dragg button is outlined and the dragged part is cut in a strange way.<br><img src="${debriefImage8}" alt='debrief image' style='width: 100%; margin-top: 10px;' />`,
+          `Search on the documentation has wierd sort order, not really relevand and the "Documentation" and "API reference" tags are not enough as we'd like to filter blog posts out as well.<br><img src="${debriefImage9}" alt='debrief image' style='width: 100%; margin-top: 10px;' />`,
+          `Search displays a strange &lt;mark&gt; tag in the results.<br><img src="${debriefImage10}" alt='debrief image' style='width: 100%; margin-top: 10px;' />`,
+          `Search cuts spaces between words due to original text being split into separate HTML text nodes without trailing spaces (which eventually get concatenated as is).<br><img src="${debriefImage11}" alt='debrief image' style='width: 100%; margin-top: 10px;' /><img src="${debriefImage12}" alt='debrief image' style='width: 100%;' />`,
+        ],
       },
     ],
   },
