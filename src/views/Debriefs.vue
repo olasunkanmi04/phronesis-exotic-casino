6cd40503c44c09e64ab80aebb38da9e2104ccdc0
Alabi Olasunkanmi <alabiolasunkanmi@outlook.com>
2025-01-23 15:03:50 +0000

    debriefs

--- src/views/Debriefs.vue
+++ src/views/Debriefs.vue
@@ -44,21 +44,21 @@
 // import debriefImage14 from "@/assets/debriefs/14-1.png";
 // import debriefImage15 from "@/assets/debriefs/14-2.png";
 // import debriefImage16 from "@/assets/debriefs/14-3.png";
-import debriefImage17 from "@/assets/debriefs/15-1.png";
-import debriefImage18 from "@/assets/debriefs/15-2.png";
-import debriefImage19 from "@/assets/debriefs/15-3.mov";
-import debriefImage20 from "@/assets/debriefs/16-1.png";
-import debriefImage21 from "@/assets/debriefs/16-2.png";
-import debriefImage22 from "@/assets/debriefs/16-3.png";
-import debriefImage23 from "@/assets/debriefs/16-4.png";
-import debriefImage24 from "@/assets/debriefs/17-1.png";
-import debriefImage25 from "@/assets/debriefs/17-2.png";
-import debriefImage26 from "@/assets/debriefs/17-3.png";
-import debriefImage27 from "@/assets/debriefs/17-4.png";
-import debriefImage28 from "@/assets/debriefs/17-5.png";
-import debriefImage29 from "@/assets/debriefs/18-1.png";
-import debriefImage30 from "@/assets/debriefs/19-1.png";
-import debriefImage31 from "@/assets/debriefs/19-2.png";
+// import debriefImage17 from "@/assets/debriefs/15-1.png";
+// import debriefImage18 from "@/assets/debriefs/15-2.png";
+// import debriefImage19 from "@/assets/debriefs/15-3.mov";
+// import debriefImage20 from "@/assets/debriefs/16-1.png";
+// import debriefImage21 from "@/assets/debriefs/16-2.png";
+// import debriefImage22 from "@/assets/debriefs/16-3.png";
+// import debriefImage23 from "@/assets/debriefs/16-4.png";
+// import debriefImage24 from "@/assets/debriefs/17-1.png";
+// import debriefImage25 from "@/assets/debriefs/17-2.png";
+// import debriefImage26 from "@/assets/debriefs/17-3.png";
+// import debriefImage27 from "@/assets/debriefs/17-4.png";
+// import debriefImage28 from "@/assets/debriefs/17-5.png";
+// import debriefImage29 from "@/assets/debriefs/18-1.png";
+// import debriefImage30 from "@/assets/debriefs/19-1.png";
+// import debriefImage31 from "@/assets/debriefs/19-2.png";
 import debriefImage32 from "@/assets/debriefs/20-1.png";
 import debriefImage33 from "@/assets/debriefs/20-2.png";
 import debriefImage34 from "@/assets/debriefs/20-3.png";
@@ -70,6 +70,8 @@ import debriefImage39 from "@/assets/debriefs/21-2.png";
 import debriefImage40 from "@/assets/debriefs/21-3.png";
 import debriefImage41 from "@/assets/debriefs/21-4.png";
 import debriefImage42 from "@/assets/debriefs/22-1.png";
+import debriefImage43 from "@/assets/debriefs/23-1.png";
+import debriefImage44 from "@/assets/debriefs/23-2.png";
 import { ref } from "vue";
 const projectFeedback = ref([
   // {
@@ -219,118 +221,118 @@ const projectFeedback = ref([
   //     },
   //   ],
   // },
-  {
-    title: "Project 15",
-    questions: [
-      {
-        question: "Which teammates executed the project?",
-        answer: "Team members: Olasunkanmi, and Bartlomiej",
-      },
-      {
-        question: "What made you smile?",
-        answer: "Documentation seem pretty straightforward.",
-      },
-      {
-        question: "What did you find confusing? Did you encounter any issues?",
-        answer: [
-          `We're not sure if Rebilly has control over this but the text color is hard to read because it's dark color on a dark background. <br><img src="${debriefImage17}" alt='debrief image' style='width: 100%; margin-top: 10px;' />`,
-          "We were confused as to how the system determines which gateway account to use. We created a separate gateway account for CAD that has 3DS enabled but the system used the previous gateway where we enabled DCC, so we had to remove CAD from the DCC enabled gateway for it to point to 3DS enabled gateway.",
-          "Related to number 2 above, we enabled CAD for the DCC and one of our visa card uses the 3DS enabled gateway and the other uses the DCC enabled gateway, we are not sure how it determines which gateway to use.",
-          `Choose another payment method button takes us out of our site. <br><img src="${debriefImage18}" alt='debrief image' style='width: 100%; margin-top: 10px;' />`,
-          `We noticed when testing, if you select a card, enter your cvv but change your mind and select another card and enter cvv for that card, it throws an error about billing address but when you start over and go straight to select the card throwing the error, it works fine.<br><video width="100%" controls>
-  <source src="${debriefImage19}" type="video/mp4">
-  Your browser does not support HTML video.
-</video>`,
-        ],
-      },
-    ],
-  },
-  {
-    title: "Project 16",
-    questions: [
-      {
-        question: "Which teammates executed the project?",
-        answer: "Team members: Olasunkanmi, and Bartlomiej",
-      },
-      {
-        question: "What made you smile?",
-        answer: "Documentation seem pretty straightforward.",
-      },
-      {
-        question: "What did you find confusing? Did you encounter any issues?",
-        answer: [
-          `Typographical error when testing on the website "anogther instead of another". We realised later on that this might have been the organization name. <br><img src="${debriefImage20}" alt='debrief image' style='width: 100%; margin-top: 10px;' />`,
-          `We had two errors on two attempt to make a payout request. One of the error was about permission and when you check the network, the error when this endpoint <em>/storefront/organizations/phronesis-exotic-escapes/payout-requests/{request_id}</em> is called had to do with ACL permissions, we updated the permissions and got the error in the image below but when we checked recomm we found both payout request attempt and we were able to fully fulfil one and partially fully the other. <br><img src="${debriefImage21}" alt='debrief image' style='width: 100%; margin-top: 10px;' /><br><img src="${debriefImage22}" alt='debrief image' style='width: 100%; margin-top: 10px;' />`,
-          `After getting a success message that allocation was successful, we immiedately got this error  <em>The total allocated amount must be less than or equal to the available amount.</em>. Also, there were two toast success notifications.". <br><img src="${debriefImage23}" alt='debrief image' style='width: 100%; margin-top: 10px;' />`,
-        ],
-      },
-    ],
-  },
-  {
-    title: "Project 17",
-    questions: [
-      {
-        question: "Which teammates executed the project?",
-        answer: "Team members: Olasunkanmi, and Dara",
-      },
-      {
-        question: "What made you smile?",
-        answer: "Documentation seem pretty straightforward.",
-      },
-      {
-        question: "What did you find confusing? Did you encounter any issues?",
-        answer: [
-          `We have eCheck, ACH and Skrill set up but only Skrill and Bank account show. <br><img src="${debriefImage24}" alt='debrief image' style='width: 100%; margin-top: 10px;' />`,
-          `Ready to payout api returns eCheck but instrument doesn't display it for either USA or Canada. <br><img src="${debriefImage25}" alt='debrief image' style='width: 100%; margin-top: 10px;' />`,
-          `In recomm the payout request page should prepoluated the amount on the request instrument since we already know that. <br><img src="${debriefImage26}" alt='debrief image' style='width: 100%; margin-top: 10px;' />`,
-          `Try to fulfil $5 out of $10 requested. Got an error and transaction was declined yet I payout request status changed to partially fulfil and total payout allocation remaining is now $5. <br><img src="${debriefImage27}" alt='debrief image' style='width: 100%; margin-top: 10px;' /> <br><img src="${debriefImage28}" alt='debrief image' style='width: 100%; margin-top: 10px;' />`,
-          `Will be nice to see the transaction/timeline in the payout request page.`,
-        ],
-      },
-    ],
-  },
-  {
-    title: "Project 18",
-    questions: [
-      {
-        question: "Which teammates executed the project?",
-        answer: "Team members: Olasunkanmi, and Bartlomiej",
-      },
-      {
-        question: "What made you smile?",
-        answer: "",
-      },
-      {
-        question: "What did you find confusing? Did you encounter any issues?",
-        answer: [
-          `We couldn't find documentation on how to configure apple pay or google pay outside of framepay.`,
-          `When we tried paying with apple pay it says Amount Pending. <br><img src="${debriefImage29}" alt='debrief image' style='width: 100%; margin-top: 10px;' />`,
-          `Is there a way the digital wallets know it's a test transaction? We thought because it's a testprocessor gateway it will know it's a test transaction.`,
-        ],
-      },
-    ],
-  },
-  {
-    title: "Project 19",
-    questions: [
-      {
-        question: "Which teammates executed the project?",
-        answer: "Team members: Olasunkanmi, and Bartlomiej",
-      },
-      {
-        question: "What made you smile?",
-        answer: "",
-      },
-      {
-        question: "What did you find confusing? Did you encounter any issues?",
-        answer: [
-          `We weren't sure what to search for in the docs. We eventually figured it out without the docs.`,
-          `We expect when two rules and conditions are met, both should take action but Skrill payment only displays when currency is USD and customer LTV is $10,000+. <br><img src="${debriefImage30}" alt='debrief image' style='width: 100%; margin-top: 10px;' /><br><img src="${debriefImage31}" alt='debrief image' style='width: 100%; margin-top: 10px;' />`,
-          "We noticed we couldn't edit a segment that was created.",
-        ],
-      },
-    ],
-  },
+  //   {
+  //     title: "Project 15",
+  //     questions: [
+  //       {
+  //         question: "Which teammates executed the project?",
+  //         answer: "Team members: Olasunkanmi, and Bartlomiej",
+  //       },
+  //       {
+  //         question: "What made you smile?",
+  //         answer: "Documentation seem pretty straightforward.",
+  //       },
+  //       {
+  //         question: "What did you find confusing? Did you encounter any issues?",
+  //         answer: [
+  //           `We're not sure if Rebilly has control over this but the text color is hard to read because it's dark color on a dark background. <br><img src="${debriefImage17}" alt='debrief image' style='width: 100%; margin-top: 10px;' />`,
+  //           "We were confused as to how the system determines which gateway account to use. We created a separate gateway account for CAD that has 3DS enabled but the system used the previous gateway where we enabled DCC, so we had to remove CAD from the DCC enabled gateway for it to point to 3DS enabled gateway.",
+  //           "Related to number 2 above, we enabled CAD for the DCC and one of our visa card uses the 3DS enabled gateway and the other uses the DCC enabled gateway, we are not sure how it determines which gateway to use.",
+  //           `Choose another payment method button takes us out of our site. <br><img src="${debriefImage18}" alt='debrief image' style='width: 100%; margin-top: 10px;' />`,
+  //           `We noticed when testing, if you select a card, enter your cvv but change your mind and select another card and enter cvv for that card, it throws an error about billing address but when you start over and go straight to select the card throwing the error, it works fine.<br><video width="100%" controls>
+  //   <source src="${debriefImage19}" type="video/mp4">
+  //   Your browser does not support HTML video.
+  // </video>`,
+  //         ],
+  //       },
+  //     ],
+  //   },
+  //   {
+  //     title: "Project 16",
+  //     questions: [
+  //       {
+  //         question: "Which teammates executed the project?",
+  //         answer: "Team members: Olasunkanmi, and Bartlomiej",
+  //       },
+  //       {
+  //         question: "What made you smile?",
+  //         answer: "Documentation seem pretty straightforward.",
+  //       },
+  //       {
+  //         question: "What did you find confusing? Did you encounter any issues?",
+  //         answer: [
+  //           `Typographical error when testing on the website "anogther instead of another". We realised later on that this might have been the organization name. <br><img src="${debriefImage20}" alt='debrief image' style='width: 100%; margin-top: 10px;' />`,
+  //           `We had two errors on two attempt to make a payout request. One of the error was about permission and when you check the network, the error when this endpoint <em>/storefront/organizations/phronesis-exotic-escapes/payout-requests/{request_id}</em> is called had to do with ACL permissions, we updated the permissions and got the error in the image below but when we checked recomm we found both payout request attempt and we were able to fully fulfil one and partially fully the other. <br><img src="${debriefImage21}" alt='debrief image' style='width: 100%; margin-top: 10px;' /><br><img src="${debriefImage22}" alt='debrief image' style='width: 100%; margin-top: 10px;' />`,
+  //           `After getting a success message that allocation was successful, we immiedately got this error  <em>The total allocated amount must be less than or equal to the available amount.</em>. Also, there were two toast success notifications.". <br><img src="${debriefImage23}" alt='debrief image' style='width: 100%; margin-top: 10px;' />`,
+  //         ],
+  //       },
+  //     ],
+  //   },
+  //   {
+  //     title: "Project 17",
+  //     questions: [
+  //       {
+  //         question: "Which teammates executed the project?",
+  //         answer: "Team members: Olasunkanmi, and Dara",
+  //       },
+  //       {
+  //         question: "What made you smile?",
+  //         answer: "Documentation seem pretty straightforward.",
+  //       },
+  //       {
+  //         question: "What did you find confusing? Did you encounter any issues?",
+  //         answer: [
+  //           `We have eCheck, ACH and Skrill set up but only Skrill and Bank account show. <br><img src="${debriefImage24}" alt='debrief image' style='width: 100%; margin-top: 10px;' />`,
+  //           `Ready to payout api returns eCheck but instrument doesn't display it for either USA or Canada. <br><img src="${debriefImage25}" alt='debrief image' style='width: 100%; margin-top: 10px;' />`,
+  //           `In recomm the payout request page should prepoluated the amount on the request instrument since we already know that. <br><img src="${debriefImage26}" alt='debrief image' style='width: 100%; margin-top: 10px;' />`,
+  //           `Try to fulfil $5 out of $10 requested. Got an error and transaction was declined yet I payout request status changed to partially fulfil and total payout allocation remaining is now $5. <br><img src="${debriefImage27}" alt='debrief image' style='width: 100%; margin-top: 10px;' /> <br><img src="${debriefImage28}" alt='debrief image' style='width: 100%; margin-top: 10px;' />`,
+  //           `Will be nice to see the transaction/timeline in the payout request page.`,
+  //         ],
+  //       },
+  //     ],
+  //   },
+  //   {
+  //     title: "Project 18",
+  //     questions: [
+  //       {
+  //         question: "Which teammates executed the project?",
+  //         answer: "Team members: Olasunkanmi, and Bartlomiej",
+  //       },
+  //       {
+  //         question: "What made you smile?",
+  //         answer: "",
+  //       },
+  //       {
+  //         question: "What did you find confusing? Did you encounter any issues?",
+  //         answer: [
+  //           `We couldn't find documentation on how to configure apple pay or google pay outside of framepay.`,
+  //           `When we tried paying with apple pay it says Amount Pending. <br><img src="${debriefImage29}" alt='debrief image' style='width: 100%; margin-top: 10px;' />`,
+  //           `Is there a way the digital wallets know it's a test transaction? We thought because it's a testprocessor gateway it will know it's a test transaction.`,
+  //         ],
+  //       },
+  //     ],
+  //   },
+  //   {
+  //     title: "Project 19",
+  //     questions: [
+  //       {
+  //         question: "Which teammates executed the project?",
+  //         answer: "Team members: Olasunkanmi, and Bartlomiej",
+  //       },
+  //       {
+  //         question: "What made you smile?",
+  //         answer: "",
+  //       },
+  //       {
+  //         question: "What did you find confusing? Did you encounter any issues?",
+  //         answer: [
+  //           `We weren't sure what to search for in the docs. We eventually figured it out without the docs.`,
+  //           `We expect when two rules and conditions are met, both should take action but Skrill payment only displays when currency is USD and customer LTV is $10,000+. <br><img src="${debriefImage30}" alt='debrief image' style='width: 100%; margin-top: 10px;' /><br><img src="${debriefImage31}" alt='debrief image' style='width: 100%; margin-top: 10px;' />`,
+  //           "We noticed we couldn't edit a segment that was created.",
+  //         ],
+  //       },
+  //     ],
+  //   },
   {
     title: "Project 20",
     questions: [
@@ -407,6 +409,26 @@ const projectFeedback = ref([
           "We don't know if the limits work correctly. The downtime was sometimes applied, but it seemed like it either took much more than the specified limit, or there was a significant delay in triggering it. Also, even when removing the downtime, we couldn't get the originally prioritized gateway to work again.",
           "It seems like changing the limit resets the counter",
           "There's no way to check the current limit usage on the gateway",
+        ],
+      },
+    ],
+  },
+  {
+    title: "Project 23",
+    questions: [
+      {
+        question: "Which teammates executed the project?",
+        answer: "Team members: Olasunkanmi, and Radoslaw",
+      },
+      {
+        question: "What made you smile?",
+        answer: "",
+      },
+      {
+        question: "What did you find confusing? Did you encounter any issues?",
+        answer: [
+          `There should be a way to ping a gateway to check if configuration is correct. <br><img src="${debriefImage44}" alt='debrief image' style='width: 100%; margin-top: 10px;' />`,
+          `Processing channel id is required on checkout.com end but when creating the gateway on Rebilly end it wasn't required.<br><img src="${debriefImage43}" alt='debrief image' style='width: 100%; margin-top: 10px;' />`,
         ],
       },
     ],
